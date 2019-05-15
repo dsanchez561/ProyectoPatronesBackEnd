fa0684fe6c9665feb2d5f1b308c07c04fcb6d540
WO\DANIELSANCHEZ <dsanchez561@hotmail.com>
2019-05-15 15:30:55 -0500

    COMMIT: commit inicial

---
+++ src/main/java/co/com/patrones/proyecto/patrones/PatronesApplication.java
@@ -0,0 +1,13 @@
+package co.com.patrones.proyecto.patrones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatronesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatronesApplication.class, args);
+	}
+
+}
